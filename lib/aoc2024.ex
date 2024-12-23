b3e7610e15787669d7012bc2c4a5e24173a58fcd
Jiahua Zhang <jzhang@selectrehab.com>
2024-12-23 14:14:13 -0600

    Basic input file handling

--- lib/aoc2024.ex
+++ lib/aoc2024.ex
@@ -4,15 +4,21 @@ defmodule Aoc2024 do
   """
 
   @doc """
-  Hello world.
+  Loads the input file for a given day
 
   ## Examples
 
-      iex> Aoc2024.hello()
+      iex> Aoc2024.load(1)
       :world
 
   """
-  def hello do
-    :world
+  def load(day) do
+    filename = day |> to_string |> String.pad_leading(2, "0") |> then(&"#{&1}.txt")
+    path = Path.join([File.cwd!(), "input", filename])
+
+    case File.read(path) do
+      {:ok, body} -> String.split(body)
+      {:error, :enoent} -> :missing
+    end
   end
 end
